0896fb1eb6c213183211e4e119ed95fd39f3c6ab
SimonDebray <simon.debray@devinci.fr>
2018-07-26 10:32:36 +0200

    add farm list

---
+++ src/components/Display.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>Your farm</h2>
+    <ul>
+      <display-item v-for="item in items"
+                 :key="item.id"
+                 v-bind:item="item"
+      ></display-item>
+    </ul>
+  </div>
+</template>
+<script>
+import { mapGetters } from 'vuex'
+import DisplayItem from './DisplayItem'
+
+export default {
+  name: 'Display',
+  components: {DisplayItem},
+  computed: {
+    ...mapGetters([
+      'items'
+    ])
+  }
+}
+</script>
+
+<style scoped>
+ul, li {
+  list-style: none;
+}
+</style>
---
+++ src/components/DisplayItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <li>
+    <h3>{{ item.name }} Farm</h3>
+    <ul>
+      <li v-for="index in item.count" v-bind:key="index">
+        <img :src="item.img"/>
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'DisplayItem',
+  props: ['item']
+}
+</script>
+
+<style scoped>
+  img {
+    height: 20px;
+    width: auto;
+  }
+  ul, li {
+    list-style: none;
+  }
+</style>
