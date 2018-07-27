04e67e10bea25f35e33120074f8db88fe0d3aad2
SimonDebray <simon.debray@devinci.fr>
2018-07-27 13:20:46 +0200

    fix unit test config

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -8,7 +8,9 @@ module.exports = {
     'vue'
   ],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/test/unit/assetsTransformer.js',
+    '\\.(css|less)$': '<rootDir>/test/unit/assetsTransformer.js'
   },
   transform: {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
@@ -18,7 +20,7 @@ module.exports = {
     '<rootDir>/test/e2e'
   ],
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
-  setupFiles: ['<rootDir>/test/unit/setup'],
+  setupFiles: ['<rootDir>/test/unit/setup', '<rootDir>/test/unit/browserMocks'],
   mapCoverage: true,
   coverageDirectory: '<rootDir>/test/unit/coverage',
   collectCoverageFrom: [
